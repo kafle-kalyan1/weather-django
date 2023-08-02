6cd7aeb789d0791e823f6164973f22cb0589feb9
unknown <kalyankafle123@gmail.com>
2023-08-02 21:38:35 +0545

    API removed

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 import requests
 import pip._vendor.requests 
+import os
+from dotenv import load_dotenv
 from django.shortcuts import render
+load_dotenv()
 
-API_KEY = 'afcde0ca355ba4fe4fc0947b41892bf0'
-
+API_KEY = os.environ.get('API_KEY')
+print(API_KEY)
 def get_weather_data(city):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
@@ -13,7 +16,7 @@ def get_weather_data(city):
 def home(request):
     if 'location' in request.GET:
         location = request.GET['location'] 
-        print (location)
+        # print (location)
         weather_data = get_weather_data(location)
         context = {'weather_data': weather_data, 'location': location}
         return render(request, 'core/index.html', context)
@@ -21,5 +24,5 @@ def home(request):
         default_location = "Nepal"
         weather_data = get_weather_data(default_location)
         context = {'weather_data': weather_data, 'location': default_location}
-        print(context)
+        # print(context)
         return render(request, 'core/index.html', context)
