12d862a5ee2062e768c3ff7663d11a8bd0c1d4f3
unknown <kalyankafle123@gmail.com>
2023-08-02 21:33:15 +0545

    Completed

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 API_KEY = 'afcde0ca355ba4fe4fc0947b41892bf0'
 
 def get_weather_data(city):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
     return data
@@ -18,7 +18,6 @@ def home(request):
         context = {'weather_data': weather_data, 'location': location}
         return render(request, 'core/index.html', context)
     else:
-        # If no location is provided, show default location
         default_location = "Nepal"
         weather_data = get_weather_data(default_location)
         context = {'weather_data': weather_data, 'location': default_location}
